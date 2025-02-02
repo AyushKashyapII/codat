7b3e68a42a7d5eb88adbc7799097c95fd3eca993
mrinalgaur2005 <mrinalgaur30@gmail.com>
2025-02-03 01:59:28 +0530

    completed the clerk implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const Page = () => {
@@ -7,6 +8,7 @@ const Page = () => {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
       <h1>Welcome to My Next.js Page</h1>
       <p>This is a simple `page.tsx` template.</p>
+      <UserButton/>
     </div>
   );
 };
